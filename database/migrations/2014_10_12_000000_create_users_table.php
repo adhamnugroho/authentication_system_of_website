bf6677830bf5fc4e86ef28a9deecaa48a00b9c9f
Adham Nugroho <adhamngroho9@gmail.com>
2023-04-10 09:09:24 +0700

    migration done

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nama_lengkap');
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->enum('status_pengguna', ['Aktif', 'Non-Aktif'])->nullable();
+            $table->enum('role', ['Admin', 'User'])->nullable();
             $table->timestamps();
         });
     }
---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::insert("INSERT INTO users (id, nama_lengkap, username, email, password, status_pengguna, role) VALUES
+        (1, 'Admin', 'admin', 'admin@gmail.com', '". bcrypt("admin") ."', 'Aktif', 'Admin'),
+        (2, 'User Test', 'usertest', 'usertest@gmail.com', '". bcrypt("pengguna") ."', 'Aktif', 'User');
+        ");
+    }
+}
